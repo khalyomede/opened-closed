9a01978267f0bbe8039262aa455d9390920ec445
Khalyomede <khalyomede@gmail.com>
2020-01-01 18:53:22 +0100

    using more modern tools to build the files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,8 @@
 import { src, dest, watch, series } from "gulp";
 import typescript from "gulp-typescript";
 import rename from "gulp-rename";
-import uglify from "gulp-uglify-es";
 import babel from "gulp-babel";
+import browserify from "gulp-bro";
 
 const lib = () =>
 	src("src/main.ts")
@@ -16,26 +16,36 @@ const lib = () =>
 
 const dist = () =>
 	src("src/opened-closed.ts")
-		.pipe(typescript())
 		.pipe(
-			babel({
-				presets: ["env"],
+			browserify({
+				transform: ["babelify"],
+				plugin: ["tsify"],
 			})
 		)
 		.pipe(
 			rename({
-				basename: "opened-closed",
+				extname: ".js",
 			})
 		)
-		.pipe(dest("dist"))
-		.pipe(uglify())
+		.pipe(dest("dist"));
+
+const distMin = () =>
+	src("src/opened-closed.ts")
+		.pipe(
+			browserify({
+				transform: ["babelify"],
+				plugin: ["tsify", "tinyify"],
+			})
+		)
 		.pipe(
 			rename({
 				suffix: ".min",
+				extname: ".js",
 			})
 		)
 		.pipe(dest("dist"));
 
 const start = () => watch("src/main.ts", series("lib"));
+const build = series(lib, dist, distMin);
 
-module.exports = { lib, dist, start };
+module.exports = { lib, dist, distMin, build };
