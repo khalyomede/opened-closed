a763ce4bbfd6abcecd7e1ef01d935c2589aad0c4
Khalyomede <khalyomede@gmail.com>
2020-01-01 18:06:23 +0100

    using ES6 in gulpfile

---
+++ gulpfile.babel.js
@@ -0,0 +1,41 @@
+import { src, dest, watch, series } from "gulp";
+import typescript from "gulp-typescript";
+import rename from "gulp-rename";
+import uglify from "gulp-uglify-es";
+import babel from "gulp-babel";
+
+const lib = () =>
+  src("src/main.ts")
+    .pipe(typescript())
+    .pipe(
+      babel({
+        presets: ["env"]
+      })
+    )
+    .pipe(dest("lib"));
+
+const dist = () =>
+  src("src/opened-closed.ts")
+    .pipe(typescript())
+    .pipe(
+      babel({
+        presets: ["env"]
+      })
+    )
+    .pipe(
+      rename({
+        basename: "opened-closed"
+      })
+    )
+    .pipe(dest("dist"))
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(dest("dist"));
+
+const start = () => watch("src/main.ts", series("lib"));
+
+module.exports = { lib, dist, start };
